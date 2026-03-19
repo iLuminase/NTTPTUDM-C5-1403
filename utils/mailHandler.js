040bc93de8af22f20eb7d6b05fe77bce676970df
Phatdevio <tienphat3968@gmail.com>
2026-03-19 13:30:08 +0700

    Add inventory management and user authentication features
    - Implement inventory routes and schema for managing product stock.
    - Update user controller to include methods for finding users by email and token.
    - Enhance product routes to create corresponding inventory records upon product creation.
    - Refactor authentication handler for improved readability and error handling.
    - Configure mail handler with credentials for sending password reset emails.

--- utils/mailHandler.js
+++ utils/mailHandler.js
@@ -1,25 +1,24 @@
 const nodemailer = require("nodemailer");
 
-
 const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 25,
-    secure: false, // Use true for port 465, false for port 587
-    auth: {
-        user: "",
-        pass: "",
-    },
+  host: "sandbox.smtp.mailtrap.io",
+  port: 25,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: "0dc99ca736b3ed",
+    pass: "0faef816fb86a7",
+  },
 });
 module.exports = {
-    sendMail: async function (to,url) {
-        const info = await transporter.sendMail({
-            from: 'hehehe@gmail.com',
-            to: to,
-            subject: "reset password URL",
-            text: "click vao day de doi pass", // Plain-text version of the message
-            html: "click vao <a href="+url+">day</a> de doi pass", // HTML version of the message
-        });
+  sendMail: async function (to, url) {
+    const info = await transporter.sendMail({
+      from: "hehehe@gmail.com",
+      to: to,
+      subject: "reset password URL",
+      text: "click vao day de doi pass", // Plain-text version of the message
+      html: "click vao <a href=" + url + ">day</a> de doi pass", // HTML version of the message
+    });
 
-        console.log("Message sent:", info.messageId);
-    }
-}
+    console.log("Message sent:", info.messageId);
+  },
+};
